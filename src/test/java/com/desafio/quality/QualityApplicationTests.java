8472c0bb8c84cf826fe1e7f8159fec7b65e85c71
stvicente <stefanie.vicente@mercadolivre.com>
2021-06-10 15:48:50 -0300

    first commit

---
+++ src/test/java/com/desafio/quality/QualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
